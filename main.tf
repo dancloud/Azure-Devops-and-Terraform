be4b6fe555e99e8124ca4184574648a3d2529db0
Daniel Pascual <dancloudx@hotmail.com>
2021-11-17 00:56:43 -0600

    Fixed our pipeline

--- main.tf
+++ main.tf
@@ -18,6 +18,11 @@ provider "azurerm" {
   features {}
 }
 
+variable "imagebuild" {
+  type = string
+  description = "Latest Image Build"
+}
+
 resource "azurerm_resource_group" "tf_test" {
   name     = "tfmainrg"
   location = "eastus"
@@ -34,7 +39,7 @@ resource "azurerm_container_group" "tfcg_test" {
 
   container {
     name   = "weatherapi"
-    image  = "dancloud/weatherapi"
+    image  = "dancloud/weatherapi:${var.imagebuild}"
     cpu    = "1"
     memory = "1"
 
