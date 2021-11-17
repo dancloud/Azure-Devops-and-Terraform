30c59be9af25ec19bb70c92a825da11350e8de8c
Daniel Pascual <dancloudx@hotmail.com>
2021-11-17 00:06:13 -0600

    added main.tf

---
+++ main.tf
@@ -0,0 +1,41 @@
+terraform {
+  required_version = ">= 1.0.9, < 1.10"
+  required_providers {
+    azurerm = {
+      source  = "hashicorp/azurerm"
+      version = "2.85.0"
+    }
+  }
+}
+
+provider "azurerm" {
+  features {}
+}
+
+resource "azurerm_resource_group" "tf_test" {
+  name     = "tfmainrg"
+  location = "eastus"
+}
+
+resource "azurerm_container_group" "tfcg_test" {
+  name                = "weatherapi"
+  location            = azurerm_resource_group.tf_test.location
+  resource_group_name = azurerm_resource_group.tf_test.name
+
+  ip_address_type = "public"
+  dns_name_label  = "dancloudwa"
+  os_type         = "Linux"
+
+  container {
+    name   = "weatherapi"
+    image  = "dancloud/weatherapi"
+    cpu    = "1"
+    memory = "1"
+
+    ports {
+      port     = 80
+      protocol = "TCP"
+    }
+  }
+
+}
